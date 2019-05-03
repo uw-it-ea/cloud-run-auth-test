587c7738c3e787bbce32d874341de289d410f70e
Maxime Deravet <maximed@uw.edu>
2019-05-03 16:00:32 -0700

    Save access to firebase. Log trace to stackdriver

--- my-test-api/src/app.js
+++ my-test-api/src/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const tracer = require('@google-cloud/trace-agent').start();
+const Firestore = require('@google-cloud/firestore');
 const express = require('express');
 
 // Constants
@@ -33,8 +35,8 @@ const app = express();
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+  const authSpan = tracer.createChildSpan({ name: 'validate-auth' });
   const authorization = req.headers["authorization"];
   if (!authorization) {
     res.type('application/json')
@@ -59,19 +61,36 @@ router.get('/', (req, res) => {
     return;
   }
 
-  verify(token).then((authToken) => {
+  try {
+    const authToken = await verify(token);
+    authSpan.endSpan();
     console.log('valid user token : ' + JSON.stringify(authToken));
-
     const userInfo = {
       name: authToken.name,
       email: authToken.email,
       picture: authToken.picture,
     }
 
+    const firestore = new Firestore();
+
+    const document = firestore.collection('my-test-api').doc(authToken.email);
+    console.log('Document created');
+    await document.set({
+      email: authToken.email,
+      lastLogin: Firestore.Timestamp.fromDate(new Date()),
+    },{merge: true});
+
+    await document.update({
+      access_count: Firestore.FieldValue.increment(1)
+    });
+
+
     res.type('application/json')
     res.send(JSON.stringify(userInfo));
     return;
-  }).catch(function (error) {
+  }
+  catch (error) {
+    authSpan.endSpan();
     console.error(error);
 
     res.type('application/json')
@@ -79,7 +98,7 @@ router.get('/', (req, res) => {
       error: error
     }));
     return;
-  })
+  }
 
 
 });
